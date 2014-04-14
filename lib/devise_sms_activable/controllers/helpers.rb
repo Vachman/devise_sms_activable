b74136687fa3562eb01c1fd656f3e386415ba99b
va4 <va4@me.com>
2014-04-14 15:11:59 +0400

    geberators fix

--- lib/devise_sms_activable/controllers/helpers.rb
+++ lib/devise_sms_activable/controllers/helpers.rb
@@ -3,9 +3,9 @@ module DeviseSmsActivable::Controllers::Helpers
   
   # Convenience helper to che ck if user has confirmed the token (and the phone) or not.
   def require_sms_activated!
-    puts 'Require_sms_activated!'
+    puts 'SMS AUTH!'
     if( send(:"authenticate_#{resource_name}!") )
-      res=send(:"current_#{resource_name}")
+      res = send(:"current_#{resource_name}")
       puts "res.confirmed_sms? #{res.confirmed_sms?}"
       redirect_to insert_sms_activation_path(res) if (!res) || (!res.confirmed_sms?)
     end
--- lib/generators/devise_sms_activable/views_generator.rb
+++ lib/generators/devise_sms_activable/views_generator.rb
@@ -2,9 +2,17 @@ require 'generators/devise/views_generator'
 
 module DeviseSmsActivable
   module Generators
-    class ViewsGenerator < Devise::Generators::ViewsGenerator
-      source_root File.expand_path("../../../../app/views", __FILE__)
+    class ViewsGenerator < Rails::Generators::Base
+      source_root File.expand_path("../../../../app/views/devise", __FILE__)
       desc 'Copies all DeviseSmsActivable views to your application.'
+      
+      def copy_views
+        directory 'sms_activations', target_path
+      end 
+      
+      def target_path
+        "app/views/devise/sms_activations"
+      end
     end
   end
 end
